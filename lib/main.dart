936764621aae3cc38cc0450c00b205ba3f6bb5f6
Anand-PS <anandpsadanandan@gmail.com>
2021-05-18 17:40:28 +0530

    Functioning Main screen Ready

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+void main() {
+  runApp(
+    MaterialApp(
+      title: "QR APP",
+      home: Scaffold(
+        body: Container(
+          child: Text('QR APP'),
+        ),
+      ),
+    ),
+  );
+}

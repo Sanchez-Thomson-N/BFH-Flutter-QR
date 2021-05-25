b08743163d3395b340333acb975e5c119a4d3fd4
gauthamv2001 <mdl19ee024@mec.ac.in>
2021-05-25 12:42:14 +0530

    login page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,184 +1,132 @@
-import "package:flutter/material.dart";
+import 'package:bfh_qr/Homepage.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Foot Mark",
-    home: HomeScreen(),
-  ));
+  runApp(
+    MaterialApp(
+      title: "Foot Mark",
+      home: SigninPage(),
+    ),
+  );
 }
 
-class HomeScreen extends StatelessWidget {
+class SigninPage extends StatefulWidget {
+  @override
+  _SigninPageState createState() => _SigninPageState();
+}
+
+class _SigninPageState extends State<SigninPage> {
   @override
   Widget build(BuildContext context) {
-    var children;
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Foot Mark"),
-          backgroundColor: Colors.lightBlue.shade700,
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              DrawerHeader(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(colors: <Color>[
-                  Colors.lightBlue.shade700,
-                  Colors.lightBlueAccent.shade400
-                ])),
-                child: Container(
-                  child: Column(
-                    children: <Widget>[
-                      Material(
-                        borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                        elevation: 10,
-                        child: Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Image.asset(
-                            'images/guest-user.jpg',
-                            width: 80,
-                            height: 80,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'Username',
-                          style: TextStyle(color: Colors.white, fontSize: 20.0),
-                        ),
-                      )
-                    ],
+    return new Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
+            Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                  child: Text(
+                    'Foot ',
+                    style:
+                        TextStyle(fontSize: 80.0, fontWeight: FontWeight.bold),
                   ),
                 ),
-              ),
-              CustomListTile(Icons.person, 'Profile', () => {}),
-              CustomListTile(Icons.edit, 'Edit profile', () => {}),
-              CustomListTile(Icons.qr_code, 'Generate QR code', () => {}),
-              CustomListTile(Icons.info, 'About Us', () => {}),
-            ],
-          ),
-        ),
-        body: Center(
-            child: InkWell(
-                onTap: () => null,
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 200,
-                    ),
-                    Container(
-                        width: 300.0,
-                        decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                  offset: Offset(0.0, 20.0),
-                                  blurRadius: 30.0,
-                                  color: Colors.black12)
-                            ],
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(22.0)),
-                        child: Row(
-                          children: <Widget>[
-                            Container(
-                              width: 200.0,
-                              height: 80,
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 12.0, horizontal: 12.0),
-                              child: Text(
-                                'Qr Code Scanner',
-                                style: Theme.of(context).textTheme.button.apply(
-                                    color: Colors.white, fontSizeDelta: 12.0),
-                              ),
-                              decoration: BoxDecoration(
-                                  color: Colors.lightBlue.shade700,
-                                  borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(95.0),
-                                      topLeft: Radius.circular(95.0),
-                                      bottomRight: Radius.circular(200.0))),
-                            ),
-                            Icon(Icons.qr_code_scanner, size: 50.0),
-                          ],
-                        )),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                        width: 300.0,
-                        decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                  offset: Offset(0.0, 20.0),
-                                  blurRadius: 30.0,
-                                  color: Colors.black12)
-                            ],
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(22.0)),
-                        child: Row(
-                          children: <Widget>[
-                            Container(
-                              width: 200.0,
-                              height: 80,
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 12.0, horizontal: 12.0),
-                              child: Text(
-                                'Qr Code Generator',
-                                style: Theme.of(context).textTheme.button.apply(
-                                    color: Colors.white, fontSizeDelta: 12.0),
-                              ),
-                              decoration: BoxDecoration(
-                                  color: Colors.lightBlue.shade700,
-                                  borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(95.0),
-                                      topLeft: Radius.circular(95.0),
-                                      bottomRight: Radius.circular(200.0))),
-                            ),
-                            Icon(Icons.qr_code, size: 50.0),
-                          ],
-                        )),
-                  ],
-                ))));
-  }
-}
-
-class CustomListTile extends StatelessWidget {
-  IconData icon;
-  String text;
-  Function onTap;
-  CustomListTile(this.icon, this.text, this.onTap);
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border(bottom: BorderSide(color: Colors.grey.shade400)),
-        ),
-        child: InkWell(
-          splashColor: Colors.lightBlueAccent.shade400,
-          onTap: onTap,
-          child: Container(
-            height: 40,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Icon(icon),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        text,
-                        style: TextStyle(fontSize: 16.0),
-                      ),
-                    ),
-                  ],
-                ),
-                Icon(Icons.arrow_right)
+                Container(
+                  padding: EdgeInsets.fromLTRB(180.0, 125.0, 0.0, 0.0),
+                  child: Text(
+                    'Mark',
+                    style: TextStyle(
+                        fontSize: 60.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue),
+                  ),
+                )
               ],
             ),
           ),
-        ),
-      ),
-    );
+          Container(
+              padding: EdgeInsets.only(top: 35.0, left: 20.0, right: 20.0),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'EMAIL',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        // hintText: 'EMAIL',
+                        // hintStyle: ,
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.lightBlue.shade700))),
+                  ),
+                  SizedBox(height: 10.0),
+                  TextField(
+                    decoration: InputDecoration(
+                        labelText: 'PASSWORD ',
+                        labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.lightBlue.shade700))),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 50.0),
+                  Container(
+                      height: 40.0,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(20.0),
+                        shadowColor: Colors.blueAccent,
+                        color: Colors.blue,
+                        elevation: 7.0,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => HomeScreen()));
+                          },
+                          child: Center(
+                            child: Text(
+                              'SIGN IN',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat'),
+                            ),
+                          ),
+                        ),
+                      )),
+                  SizedBox(height: 20.0),
+                  Container(
+                    height: 40.0,
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.black,
+                              style: BorderStyle.solid,
+                              width: 1.0),
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Center(
+                          child: Text('EXIT',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat')),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+        ]));
   }
 }
